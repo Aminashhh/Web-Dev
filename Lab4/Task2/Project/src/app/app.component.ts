b6a7fca595867cd02568928a0be6aed0d046bbb1
Aminashh <a_muratbek@kbtu.kz>
2025-02-16 23:16:10 +0600

    Lab4

---
+++ Lab4/Task2/Project/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+// import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [ProductsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Project';
+  
+}
